709359ec4f73f34ef22df5d07f20961d13181bf1
Juan Angel <juanangel0x7c5@gmail.com>
2021-01-22 02:14:04 +0100

    fix bug EntityManager

--- src/main/java/me/formacion/Config.java
+++ src/main/java/me/formacion/Config.java
@@ -1,11 +1,36 @@
 package me.formacion;
 
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.*;
+import javax.persistence.EntityManagerFactory;
 
-//@Configuration
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
 //@EnableWebMvc
 public class Config {
-	public static final String PERSISTENCE_UNIT_NAME = "FORACION";
+    @Autowired 
+    Environment env;
+	public String PERSISTENCE_UNIT_NAME = "FORACION";
+	
+    @Bean
+	public LocalEntityManagerFactoryBean entityManagerFactory(){
+	    LocalEntityManagerFactoryBean factoryBean = new LocalEntityManagerFactoryBean();
+	    factoryBean.setPersistenceUnitName(PERSISTENCE_UNIT_NAME);
+	    return factoryBean;
+	}
+    @Bean
+    public PlatformTransactionManager txManager(EntityManagerFactory emf) {
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(emf);
+        //txManager.setDataSource(dataSource());
+        return txManager;
+    }
 
 }
